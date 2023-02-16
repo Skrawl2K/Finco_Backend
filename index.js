16a9d1ea0309c4ca540b4d3297efea016996ae52
Skrawl2K <107536782+Skrawl2K@users.noreply.github.com>
2023-02-16 11:15:06 +0100

    corrections -  token / refresh token & mail verification disabled for testing purpose - login / register basics

--- index.js
+++ index.js
@@ -11,7 +11,10 @@ import { verifyToken } from './util/token.js'
 const PORT = process.env.PORT
 const app = express()
 const formToBody = multer({ dest: './public' })
-app.use(cors())
+app.use(cors({
+    origin: true,
+    credentials: true
+}))
 app.use(morgan('dev'))
 app.use(express.json())
 app.use('/public', express.static('./public'))
