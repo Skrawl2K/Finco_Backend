5871b32f7bc7f0d3fe6fd524d5a5daadfef194d9
Skrawl2K <107536782+Skrawl2K@users.noreply.github.com>
2023-02-16 11:10:55 +0100

    token / refresh token & mail verification disabled for testing purpose - login / register basics

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,7 +1,7 @@
 import { getDb } from '../util/db.js';
 import { ObjectId } from "mongodb"
-import { createToken, verifyToken } from '../util/token.js';
-import nodemailer from 'nodemailer';
+// import { createToken, verifyToken } from '../util/token.js';
+// import nodemailer from 'nodemailer';
 
 const cookieConfig = {
     httpOnly: true,
@@ -15,21 +15,21 @@ export const loginUser = async (req, res) => {
     const db = await getDb()
     const dbUser = await db.collection('user').findOne({ email: user.email })
 
-    if (dbUser === null || dbUser.password !== user.password)
-        res.status(401).end()
-    else {
-        const token = createToken(dbUser)
-        res.cookie('token', token, cookieConfig)
+    // if (dbUser === null || dbUser.password !== user.password)
+    //     res.status(401).end()
+    // else {
+    //     const token = createToken(dbUser)
+    //     res.cookie('token', token, cookieConfig)
 
-        //Verify the Token 
-        const decoded = verifyToken(token)
-        if (decoded) {
-            res.status(200).end()
-        }
-        else {
-            res.status(401).end();
-        }
-    }
+    //     //Verify the Token 
+    //     const decoded = verifyToken(token)
+    //     if (decoded) {
+    //         res.status(200).end()
+    //     }
+    //     else {
+    //         res.status(401).end();
+    //     }
+    // }
 }
 
 //! USER - SIGNUP - POST
@@ -49,47 +49,47 @@ export const registerUser = async (req, res) => {
 }
 
 //! MAIL - VERIFICATION
-export const sendVerificationEmail = async (req, res) => {
-    const { email, name } = req.body;
+// export const sendVerificationEmail = async (req, res) => {
+//     const { email, name } = req.body;
 
-    let transporter = nodemailer.createTransport({
-        service: process.env.MAIL_SERVICE,
-        auth: {
-            user: process.env.MAIL_EMAIL,
-            pass: process.env.MAIL_PASSWORD
-        },
-    });
+//     let transporter = nodemailer.createTransport({
+//         service: process.env.MAIL_SERVICE,
+//         auth: {
+//             user: process.env.MAIL_EMAIL,
+//             pass: process.env.MAIL_PASSWORD
+//         },
+//     });
 
-    const verifyToken = crypto.randomBytes(20).toString('hex');
-    const verificationURL = `http://${req.headers.host}/users/verify?token=${verifyToken}`;
+//     const verifyToken = crypto.randomBytes(20).toString('hex');
+//     const verificationURL = `http://${req.headers.host}/users/verify?token=${verifyToken}`;
 
-    const mailHTML = `Hi ${name},<br/><br/> 
-  Please click on the link below to verify your email address: <br/><br/> 
-  <a href="${verificationURL}" target="_blank">Click Here to Verify</a><br/><br/> 
-  `;
+//     const mailHTML = `Hi ${name},<br/><br/> 
+//   Please click on the link below to verify your email address: <br/><br/> 
+//   <a href="${verificationURL}" target="_blank">Click Here to Verify</a><br/><br/> 
+//   `;
 
-    let mailOptions = {
-        to: email,
-        subject: 'Please confirm your Email address',
-        html: mailHTML
-    };
+//     let mailOptions = {
+//         to: email,
+//         subject: 'Please confirm your Email address',
+//         html: mailHTML
+//     };
 
-    await transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-            res.status(403).end();
-        }
-        else {
-            console.log('Message %s sent: %s', info.messageId, info.response);
-            User.findOneAndUpdate({ email }, { $set: { verifyToken } }, { new: true, upsert: true }, (err, doc) => {
-                if (err) res.status(500).json(err);
-                else {
-                    res.status(200).json(doc);
-                }
-            });
-        }
-    });
-};
+//     await transporter.sendMail(mailOptions, (error, info) => {
+//         if (error) {
+//             console.log(error);
+//             res.status(403).end();
+//         }
+//         else {
+//             console.log('Message %s sent: %s', info.messageId, info.response);
+//             User.findOneAndUpdate({ email }, { $set: { verifyToken } }, { new: true, upsert: true }, (err, doc) => {
+//                 if (err) res.status(500).json(err);
+//                 else {
+//                     res.status(200).json(doc);
+//                 }
+//             });
+//         }
+//     });
+// };
 
 //! TEST USER ____________________________________________________________
 //name: Skrawl2K
