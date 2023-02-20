08fa68efd334be34780dcf3a24f0cec196849dd9
Skrawl2K <107536782+Skrawl2K@users.noreply.github.com>
2023-02-20 14:32:32 +0100

    code refactored

--- controller/UserController.js
+++ controller/UserController.js
@@ -25,16 +25,15 @@ export const loginUser = async (req, res) => {
     }
 
     //! Possible source of the login error
-    // //Verify the Token 
-    // const decoded = verifyToken(token)
-    // if (decoded) {
-    //     res.status(200).end()
-    // }
-    // else {
-    //     res.status(401).end();
-    // }
+    //Verify the Token 
+    const decoded = verifyToken(token)
+    if (decoded) {
+        res.status(200).end()
+    }
+    else {
+        res.status(401).end();
+    }
 }
-
 
 //! USER - SIGNUP - POST
 export const registerUser = async (req, res) => {
@@ -52,55 +51,6 @@ export const registerUser = async (req, res) => {
     res.status(200).end()
 
 }
-
-//! MAIL - VERIFICATION
-// export const sendVerificationEmail = async (req, res) => {
-//     const { email, name } = req.body;
-
-//     let transporter = nodemailer.createTransport({
-//         service: process.env.MAIL_SERVICE,
-//         auth: {
-//             user: process.env.MAIL_EMAIL,
-//             pass: process.env.MAIL_PASSWORD
-//         },
-//     });
-
-//     const verifyToken = crypto.randomBytes(20).toString('hex');
-//     const verificationURL = `http://${req.headers.host}/users/verify?token=${verifyToken}`;
-
-//     const mailHTML = `Hi ${name},<br/><br/> 
-//   Please click on the link below to verify your email address: <br/><br/> 
-//   <a href="${verificationURL}" target="_blank">Click Here to Verify</a><br/><br/> 
-//   `;
-
-//     let mailOptions = {
-//         to: email,
-//         subject: 'Please confirm your Email address',
-//         html: mailHTML
-//     };
-
-//     await transporter.sendMail(mailOptions, (error, info) => {
-//         if (error) {
-//             console.log(error);
-//             res.status(403).end();
-//         }
-//         else {
-//             console.log('Message %s sent: %s', info.messageId, info.response);
-//             User.findOneAndUpdate({ email }, { $set: { verifyToken } }, { new: true, upsert: true }, (err, doc) => {
-//                 if (err) res.status(500).json(err);
-//                 else {
-//                     res.status(200).json(doc);
-//                 }
-//             });
-//         }
-//     });
-// };
-
-//! TEST USER ____________________________________________________________
-//name: Skrawl2K
-//email: kimkevinkoslowski@gmail.com
-//password: BLARP1234
-//!______________________________________________________________________
 
 //! USER - EDIT - PUT (transport via application/json)
 export const editUser = async (req, res) => {
@@ -144,23 +94,6 @@ export const deleteUser = async (req, res) => {
     }
 }
 
-//! WORKS______________________________
-// export const baseUser = async (req, res) => {
-//     const token = req.cookies.token
-//     const db = await getDb()
-//     try {
-//         const result = verifyToken(token)
-//         console.log(result);
-
-//         const dbUser = await db.collection('user').findOne({ _id: new ObjectId(result.email, result) });
-
-//         res.status(200).json(dbUser)
-//     } catch (err) {
-//         console.error(err)
-//         res.status(401).end()
-//     }
-// }
-//! TEST______________________________
 export const baseUser = async (req, res) => {
     const token = req.cookies.token
     const db = await getDb()
